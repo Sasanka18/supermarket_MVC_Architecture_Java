57fcf574c66951e7851a60a3e73a82b5fc4533fb
Sasanka18 <124145233+Sasanka18@users.noreply.github.com>
2024-07-20 18:43:03 +0530

    Add files via upload

---
+++ cmjd106.mvc/src/edu/ijse/mvc/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package edu.ijse.mvc;
+
+import edu.ijse.mvc.view.ItemView;
+import edu.ijse.mvc.view.OrderView;
+
+/**
+ *
+ * @author PLUSVISION PC
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+       new OrderView().setVisible(true);
+    }
+    
+}
